82aaf94b017e57a59eaf9ebba031d8dea7637df0
Kirill Eliseev <ekn62@bk.ru>
2023-05-08 14:33:15 +0300

    add input

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
+import { PromptService } from './core/prompt/prompt.service.js'
+
+
 export class App {
-	run() {
-		console.log('Done');
+	async run() {
+		const res = await (new PromptService()).input<number>('Число', 'number')
+		console.log(res);
 	}
 }
 
 const app = new App;
-
 app.run();
